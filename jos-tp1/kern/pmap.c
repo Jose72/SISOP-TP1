63a28441b0009c0e10abca6916e2f5bc0e664aa4
Jose72 <jjjtony_72@hotmail.com>
2017-04-19 10:48:20 -0300

    mem_init(): agregado código para crear el arreglo de páginas pages

--- jos-tp1/kern/pmap.c
+++ jos-tp1/kern/pmap.c
@@ -149,6 +149,10 @@ mem_init(void)
 	// to initialize all fields of each struct PageInfo to 0.
 	// Your code goes here:
 
+        //mem_init_pages+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        pages = (struct PageInfo*) boot_alloc(npages*sizeof(struct PageInfo));    
+        memset(pages, 0, npages*sizeof(struct PageInfo));
+
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
