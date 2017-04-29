66ef4b6d94ea92f3f48bf70a73d7fe5213400fb8
Jose72 <jjjtony_72@hotmail.com>
2017-04-29 16:28:26 -0300

    Tarea: mem_init_envs finalizada

--- jos-tp2/kern/pmap.c
+++ jos-tp2/kern/pmap.c
@@ -170,7 +170,9 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
-
+        envs = (struct Env*) boot_alloc(NENV*sizeof(struct Env));    
+        memset(pages, 0, NENV*sizeof(struct Env));        
+        
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
 	// up the list of free physical pages. Once we've done so, all further
@@ -203,6 +205,7 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
+        boot_map_region(kern_pgdir, UENVS, PTSIZE, PADDR(pages), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
