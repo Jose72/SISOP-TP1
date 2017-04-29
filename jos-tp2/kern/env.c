8d5948085b41817543b277209e60b9890ee24fb5
Jose72 <jjjtony_72@hotmail.com>
2017-04-29 19:03:06 -0300

    env_setup_vm() implemented

--- jos-tp2/kern/env.c
+++ jos-tp2/kern/env.c
@@ -181,6 +181,10 @@ env_setup_vm(struct Env *e)
 	//    - The functions in kern/pmap.h are handy.
 
 	// LAB 3: Your code here.
+        p->pp_ref++; //incremento ref
+        e->env_pgdir = page2kva(p); // es un kernel virtual addres
+	memcpy(e->env_pgdir, kern_pgdir, PGSIZE); 
+ 
 
 	// UVPT maps the env's own page table read-only.
 	// Permissions: kernel R, user R
