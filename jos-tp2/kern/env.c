e28432d61555922044445dd2ee22f1923e886d07
lucia <lt_caponpaul@hotmail.com>
2017-05-04 18:28:54 -0300

    Remove unneccessary validation

--- jos-tp2/kern/env.c
+++ jos-tp2/kern/env.c
@@ -509,13 +509,10 @@ env_run(struct Env *e)
 		curenv->env_status = ENV_RUNNABLE;
 	}
 
-        if (curenv != e){
-                curenv->env_status = ENV_RUNNABLE;
-        }
-        curenv = e;
-        e->env_status = ENV_RUNNING; 
-        e->env_runs++;
-        lcr3(PADDR(e->env_pgdir));
+    curenv = e;
+    e->env_status = ENV_RUNNING; 
+    e->env_runs++;
+    lcr3(PADDR(e->env_pgdir));
         
 	env_pop_tf(&e->env_tf);         
         //panic("env_run not yet implemented");
