ef53d2823002b1d20ec7f0c2bfdca40050ed1a3f
lucia <lt_caponpaul@hotmail.com>
2017-05-18 18:52:00 -0300

    implement sys_cputs

--- jos-tp2/kern/syscall.c
+++ jos-tp2/kern/syscall.c
@@ -21,7 +21,10 @@ sys_cputs(const char *s, size_t len)
 	// Destroy the environment if not.
 
 	// LAB 3: Your code here.
-
+	struct Env *e;
+	envid2env(curenv->env_id, &e, 1);
+	user_mem_assert(e, s, len, PTE_U);
+	
 	// Print the string supplied by the user.
 	cprintf("%.*s", len, s);
 }
